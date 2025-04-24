506eb0d1f70319a847636fb702c78bbb24e0b75c
flaviocm1990 <148896633+flaviocm1990@users.noreply.github.com>
2025-04-24 18:16:32 -0300

    created strings file

---
+++ constants/Strings.js
@@ -0,0 +1,24 @@
+export const TITLES = {
+    selectArea: "Seleccione Área",
+    selectTipe: "Seleccione: ",
+  };
+  
+  export const AREA_OPTIONS = [
+    { label: "U T I", icon: "hospital", value: "UTI" },
+    { label: "U C G", icon: "stethoscope", value: "UCG" },
+  ];
+  
+  export const TIPE_OPTIONS = [
+    { label: "Empleados", icon: "account", value: "empleados" },
+    { label: "Pacientes", icon: "account-group", value: "pacientes" },
+  ];
+
+  export const TOP_BAR = {
+    topBarTitleChoice: "Empleados/ Pacientes",
+    topBarTitlePatient: "Seleccione Paciente:",
+    topBarTitleEmploy: "Seleccione Empleado",
+  };
+  export const MODAL_TITLES = {
+    modalTitleEmployPatients: "Empleados/ Pacientes",
+  };
+  
